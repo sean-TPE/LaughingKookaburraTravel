e614bb0795a0fc819c46d4c7f64f4157463a94a1
sean-TPE <sean19800121@gmail.com>
2017-11-19 21:43:18 +0800

    Static Files and Views

--- kookaburra.js
+++ kookaburra.js
@@ -11,6 +11,8 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || port);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req,res) {
     res.render('home');
 });
