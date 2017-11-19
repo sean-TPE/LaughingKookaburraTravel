4eeff41840a8681bbd66555258f4887d5b5b6128
sean-TPE <sean19800121@gmail.com>
2017-11-19 12:39:07 +0800

    Introduction of Routing.

--- helloNode.js
+++ helloNode.js
@@ -3,8 +3,27 @@ var http = require('http');
 const port = 3000;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Node.js!');
+    // ignore query string
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+
+    switch (path) {
+        case '': {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Homepage');
+            break;
+        }
+        case '/about': {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Abot');
+            break;
+        }
+        default: {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Not Found');
+            break;
+        }
+    }
+    
 }).listen(port);
 
 console.log(`Server started on localhost:${port}; press Ctrl-C to terminate...`);
